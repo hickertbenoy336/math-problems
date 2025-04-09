616c86ec6370ae44baff68d3ed162d7c35916e62
hickertbenoy336 <hickertbenoy336@gmail.com>
2025-04-09 13:56:55 +0000

    Add 29.rs

---
+++ src/29.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let num = 10; // Example number
+    if num > 5 && num < 15 {
+        println!("The number is within the specified range.");
+    } else {
+        println!("The number is not within the specified range.");
+    }
+}
